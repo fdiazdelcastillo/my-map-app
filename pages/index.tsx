4a5960bcb2738e7ee4cbd222992d93a91a8ed08d
Fernando Delfín Díaz del Castillo Pabón <fdiazdelcastillo@gmail.com>
2024-05-22 14:40:30 -0500

    Inicial

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Map from "../components/Map";
+require("dotenv").config();
 
 const inter = Inter({ subsets: ["latin"] });
 
